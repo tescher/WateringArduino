2438bced9e58bf112cd52d2f7a842673f4d6960c
Tim Escher <tim@eschers.com>
2015-04-22 21:27:41 -0800

    initial commit

---
+++ WateringArduino.ino
@@ -0,0 +1,63 @@
+// Automatic watering system
+// (C) 2015 Tim Escher
+
+#define ZONES 2
+#define PAUSE 20*60*1000 // In milliseconds
+
+struct zoneConfig {
+  byte pumpHiPin;
+  byte pumpLoPin;
+  unsigned int moistureTrigger; // 0-1023
+  unsigned int pumpTime;  // In seconds
+};
+struct zoneConfig zones[ZONES];
+
+void pump_off(struct zoneConfig zone) {
+  digitalWrite(zone.pumpHiPin,LOW);
+  digitalWrite(zone.pumpLoPin,LOW);
+};
+
+void pump_on(struct zoneConfig zone) {
+  digitalWrite(zone.pumpHiPin,HIGH);
+  digitalWrite(zone.pumpLoPin,LOW);
+};
+
+void setup() {
+  // Initialize the zone configs and pumps
+  
+  byte zone;
+  
+  // Zone 0
+  zone = 0;
+  zones[zone].pumpHiPin = 3;
+  pinMode(zones[zone].pumpHiPin,OUTPUT);
+  zones[zone].pumpLoPin = 4;
+  pinMode(zones[zone].pumpLoPin,OUTPUT);
+  zones[zone].moistureTrigger = 300;
+  zones[zone].pumpTime = 20;
+  
+  // Zone 1
+  zone = 1;
+  zones[zone].pumpHiPin = 5;
+  pinMode(zones[zone].pumpHiPin,OUTPUT);
+  zones[zone].pumpLoPin = 6;
+  pinMode(zones[zone].pumpLoPin,OUTPUT);
+  zones[zone].moistureTrigger = 300;
+  zones[zone].pumpTime = 20;
+  
+  // Turn off all pumps
+  for(zone=0; zone<ZONES; zone++) {
+    pump_off(zones[zone]);
+  }
+ 
+}
+
+void loop() {
+  for (byte zone=0; zone<ZONES; zone++) {
+    pump_on(zones[zone]);
+    delay(5000);
+    pump_off(zones[zone]);
+    delay(1000);
+  }
+
+}
